548ac05b34e76d70086efbe1c5b94c40a3a14b60
Xiaohm <1276214348@qq.com>
2024-02-06 22:38:00 +0800

    main.js挂载pinia

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
-
 import App from "./App.vue";
+import { createPinia } from "pinia";
 import router from "./router";
 
 const app = createApp(App);
+const store = createPinia();
 
-app.use(createPinia());
+app.use(store);
 app.use(router);
 
 app.mount("#app");
