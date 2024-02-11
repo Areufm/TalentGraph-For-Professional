14985ba1050b540b85f8fd0620a07cfd50304c0e
Xiaohm <1276214348@qq.com>
2024-02-11 16:41:16 +0800

    更新搜索框组件

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,30 +1,48 @@
 ﻿<template>
-  <div class="input__container">
-    <div class="shadow__input"></div>
-    <button class="input__button__shadow" @click="handleSearch">
+  <form class="form">
+    <button @click="handleSearch">
       <svg
+        width="17"
+        height="16"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        height="20px"
-        width="20px"
+        role="img"
+        aria-labelledby="search"
       >
         <path
-          d="M4 9a5 5 0 1110 0A5 5 0 014 9zm5-7a7 7 0 104.2 12.6.999.999 0 00.093.107l3 3a1 1 0 001.414-1.414l-3-3a.999.999 0 00-.107-.093A7 7 0 009 2z"
-          fill-rule="evenodd"
-          fill="#17202A"
+          d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9"
+          stroke="currentColor"
+          stroke-width="1.333"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         ></path>
       </svg>
     </button>
     <input
-      v-model="searchQuery"
+      class="input"
+      placeholder="Type your text"
+      required=""
       type="text"
-      name="text"
-      class="input__search"
-      placeholder="What do you want to search?"
-      @keyup.enter="handleSearch"
+      v-model="searchQuery"
+      @keydown.enter.prevent="handleSearch"
     />
-  </div>
+    <button class="reset" type="reset">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M6 18L18 6M6 6l12 12"
+        ></path>
+      </svg>
+    </button>
+  </form>
 </template>
 
 <script>
@@ -42,75 +60,93 @@ export default {
       console.log(this.searchQuery);
       // 处理搜索事件，你可以在这里触发搜索操作
       // 例如，可以将搜索关键词通过事件或者回调函数传递给父组件
-      this.$emit("search", this.searchQuery);
+      // this.$emit("form", this.searchQuery);
     },
   },
 };
 </script>
 
 <style scoped>
-.input__container {
-  position: relative;
-  background: rgba(255, 255, 255, 0.664);
-  padding: 10px 15px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 5px;
-  border-radius: 22px;
-  width: 300px;
-  transition: transform 400ms;
-  transform-style: preserve-3d;
-  transform: rotateX(15deg) rotateY(-20deg);
-  perspective: 500px;
-}
+/* From uiverse.io by @satyamchaudharydev */
+/* removing default style of button */
 
-.shadow__input {
-  content: "";
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  left: 0;
-  bottom: 0;
-  z-index: -1;
-  filter: blur(30px);
-  border-radius: 20px;
-  background-color: #999cff;
-  background-image: radial-gradient(
-      at 85% 51%,
-      hsla(60, 60%, 61%, 1) 0px,
-      transparent 50%
-    ),
-    radial-gradient(at 74% 68%, hsla(235, 69%, 77%, 1) 0px, transparent 50%),
-    radial-gradient(at 64% 79%, hsla(284, 72%, 73%, 1) 0px, transparent 50%),
-    radial-gradient(at 75% 16%, hsla(283, 60%, 72%, 1) 0px, transparent 50%),
-    radial-gradient(at 90% 65%, hsla(153, 70%, 64%, 1) 0px, transparent 50%),
-    radial-gradient(at 91% 83%, hsla(283, 74%, 69%, 1) 0px, transparent 50%),
-    radial-gradient(at 72% 91%, hsla(213, 75%, 75%, 1) 0px, transparent 50%);
-}
-
-.input__button__shadow {
-  cursor: pointer;
+.form button {
   border: none;
   background: none;
-  transition: transform 400ms, background 400ms;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 12px;
-  padding: 5px;
+  color: #8b8ba7;
 }
-
-.input__button__shadow:hover {
-  background: rgba(255, 255, 255, 0.411);
-}
-
-.input__search {
-  width: 100%;
-  border-radius: 20px;
-  outline: none;
-  border: none;
-  padding: 8px;
+/* styling of whole input container */
+.form {
+  --timing: 0.3s;
+  --width-of-input: 200px;
+  --height-of-input: 40px;
+  --border-height: 2px;
+  --input-bg: #fff;
+  --border-color: #2f2ee9;
+  --border-radius: 30px;
+  --after-border-radius: 1px;
   position: relative;
+  width: var(--width-of-input);
+  height: var(--height-of-input);
+  display: flex;
+  align-items: center;
+  padding-inline: 0.8em;
+  border-radius: var(--border-radius);
+  transition: border-radius 0.5s ease;
+  background: var(--input-bg, #fff);
+}
+/* styling of Input */
+.input {
+  font-size: 0.9rem;
+  background-color: transparent;
+  width: 100%;
+  height: 100%;
+  padding-inline: 0.5em;
+  padding-block: 0.7em;
+  border: none;
+}
+/* styling of animated border */
+.form:before {
+  content: "";
+  position: absolute;
+  background: var(--border-color);
+  transform: scaleX(0);
+  transform-origin: center;
+  width: 100%;
+  height: var(--border-height);
+  left: 0;
+  bottom: 0;
+  border-radius: 1px;
+  transition: transform var(--timing) ease;
+}
+/* Hover on Input */
+.form:focus-within {
+  border-radius: var(--after-border-radius);
+}
+
+input:focus {
+  outline: none;
+}
+/* here is code of animated border */
+.form:focus-within:before {
+  transform: scale(1);
+}
+/* styling of close button */
+/* == you can click the close button to remove text == */
+.reset {
+  border: none;
+  background: none;
+  opacity: 0;
+  visibility: hidden;
+}
+/* close button shown when typing */
+input:not(:placeholder-shown) ~ .reset {
+  opacity: 1;
+  visibility: visible;
+}
+/* sizing svg icons */
+.form svg {
+  width: 17px;
+  margin-top: 3px;
 }
 </style>
