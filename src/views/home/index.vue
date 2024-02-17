e082ad07d5c03039a08327a48f970472fa412271
Xiaohm <1276214348@qq.com>
2024-02-17 20:35:54 +0800

    更新主页面

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,43 +2,54 @@
   <n-layout>
     <n-layout-header>
       <div class="header_left">
-        <div class="header_left_text">城市</div>
+        <div class="header_left_title">服务外包A15赛题</div>
+        <div class="nav-city">
+          <p class="nav-city-box" ka="header-switch-city">
+            <i class="icon-poi"></i><span class="nav-city-selected">杭州</span
+            ><span class="switchover-city">[切换城市]</span>
+          </p>
+          <!-- <div class="city-box">
+            <ul class="dorpdown-province"></ul>
+            <div class="dorpdown-city"></div>
+          </div> -->
+        </div>
         <div class="header_left_text">首页</div>
         <div class="header_left_text">推荐岗位</div>
       </div>
       <div class="header_right">
-        <div class="header_right_text">我要招聘</div>
-        <div class="header_right_text">我要求职</div>
-        <div
-          class="header_right_text"
-          style="border: rgb(255, 255, 255) solid 2px; border-radius: 50px"
+        <a href="/login" class="header_right_text">我要招聘</a>
+        <a href="/login" class="header_right_text">我要求职</a>
+        <button
+          style="
+            border: rgb(255, 255, 255) solid 2px;
+            border-radius: 50px;
+            background-color: rgba(255, 255, 255, 0);
+            color: white;
+            padding: 10px;
+          "
+          @click="toLogin"
         >
-          登录/注册
-        </div>
+          登录 / 注册
+        </button>
       </div>
     </n-layout-header>
     <n-layout-content content-style="padding: 24px;">
       <div class="centerPosition" style="margin-top: 20px">
         <SearchBar />
       </div>
-      <div>{{ count }}</div>
-      <div>
-        <n-button @click="count++">+</n-button>
-        <n-button @click="count--">-</n-button>
-      </div>
-      <div>
-        <button @click="toLogin">tologin</button>
-      </div>
-      <div>
-        <n-button type="primary"> Primary </n-button>
-      </div>
       <h1>热门职位</h1>
-      <div class="hotWorks">
-        <HotWorks />
+      <HotWorks />
+      <HotWorks />
+      <HotWorks />
+      <div class="get-more">
+        <GetMore />
       </div>
       <h1>热门企业</h1>
-      <div class="hotCompany">
-        <HotCompany />
+      <HotCompany />
+      <HotCompany />
+      <HotCompany />
+      <div class="get-more">
+        <GetMore />
       </div>
     </n-layout-content>
   </n-layout>
@@ -50,16 +61,69 @@ import { useRouter } from "vue-router";
 import SearchBar from "@/components/SearchBar.vue";
 import HotCompany from "@/components/HotCompany.vue";
 import HotWorks from "@/components/HotWorks.vue";
+import GetMore from "@/components/GetMore.vue";
 
 const router = useRouter();
 const count = ref(0);
 
 const toLogin = () => {
-  router.push("/login");
+  router.push("/register");
 };
 </script>
 
 <style scoped>
+.nav-city {
+  float: left;
+  position: relative;
+  cursor: pointer;
+  height: 49px;
+  line-height: 49px;
+  color: #00bebd;
+  display: flex;
+  align-items: center;
+  margin: 5px;
+}
+.nav-city .nav-city-selected {
+  display: inline-block;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 84px;
+  vertical-align: middle;
+}
+.nav-city .icon-poi {
+  display: inline-block;
+  vertical-align: top;
+  width: 18px;
+  height: 18px;
+  background: url(https://img.bosszhipin.com/static/file/2023/umua62pczi1679922532668.png)
+    0 -18px/18px auto no-repeat;
+  margin: 16px 3px 0 24px;
+}
+.nav-city .switchover-city {
+  font-size: 12px;
+  color: #fff;
+  margin-left: 10px;
+}
+.nav-city .city-box {
+  top: 49px;
+}
+
+.nav-city .city-box {
+  font-size: 14px;
+}
+.city-box {
+  display: none;
+  position: absolute;
+  width: 298px;
+  background: #fff;
+  top: 45px;
+  left: 0;
+  z-index: 1;
+  color: #9fa3b0;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+}
+
 h1 {
   text-align: center;
   margin-bottom: 20px;
@@ -92,16 +156,22 @@ h1 {
 
 .header_left {
   display: flex;
+  align-items: center;
 }
 .header_right {
   display: flex;
   margin-left: auto;
 }
 
+.header_left_title {
+  margin: 10px;
+  color: white;
+  font-size: 25px;
+  font-weight: bold;
+}
 .header_left_text {
   margin: 10px;
   color: white;
-  display: flex;
 }
 .header_right_text {
   margin: 10px, 0;
@@ -109,14 +179,22 @@ h1 {
   display: flex;
   padding: 10px;
   width: 70px;
+  text-decoration: none;
 }
 
 .n-layout-content {
   /* flex: 1; */
   height: 90%;
-  background: rgb(223, 217, 217);
+  background: rgb(225, 222, 222);
   display: flex;
   flex-direction: column;
   margin-top: 70px;
+}
+
+.get-more {
+  margin: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
