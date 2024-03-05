43703b6056d05a75fa97c489b92625cb616e099d
Xiaohm6 <1276214348@qq.com>
2024-03-05 14:56:52 +0800

    知识图谱组件

---
+++ src/components/KnowGraph.vue
@@ -0,0 +1,16 @@
+﻿<template>
+  <div class="know-graph">知识图谱</div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+</script>
+
+<style scoped>
+.know-graph {
+  width: 80%;
+  height: 300px;
+  border: 1px white solid;
+  background-color: rgba(211, 209, 209, 0.613);
+}
+</style>
