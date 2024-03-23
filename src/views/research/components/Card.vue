6549759d8304f1ad87b1d26470cba7cfb5cd465e
Xiaohm6 <1276214348@qq.com>
2024-03-23 20:54:55 +0800

    新增搜索页面

---
+++ src/views/research/components/Card.vue
@@ -0,0 +1,102 @@
+﻿<template>
+  <div class="card">
+    <div class="card-details">
+      <div style="display: flex; top: 0; justify-content: space-between">
+        <p class="work_name">工作名称</p>
+        <p class="work_salary">薪资</p>
+      </div>
+      <div style="display: flex">
+        <p style="margin: 0 10px">关键词1</p>
+        <p style="margin: 0 10px">关键词2</p>
+        <p style="margin: 0 10px">关键词3</p>
+        <p style="margin: 0 10px">关键词4</p>
+      </div>
+      <div style="display: flex; justify-content: space-between">
+        <p class="company_name">企业名</p>
+        <p class="job">职业类型</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.card {
+  /* width: 350px; */
+  /* width: 30%; */
+  height: 144px;
+  border-radius: 20px;
+  background: #f5f5f5;
+  position: relative;
+  /* padding: 1rem; */
+  border: 2px solid #c3c6ce;
+  transition: 0.5s ease-out;
+  overflow: visible;
+  margin: 5px 10px;
+}
+
+.card-details {
+  color: black;
+  height: 100%;
+  /* gap: 0.5em; */
+  display: grid;
+  align-items: center;
+  /* place-content: center; */
+}
+
+.card-button {
+  transform: translate(-50%, 125%);
+  width: 60%;
+  border-radius: 1rem;
+  border: none;
+  background-color: #008bf8;
+  color: #fff;
+  font-size: 1rem;
+  padding: 0.5rem 1rem;
+  position: absolute;
+  left: 50%;
+  bottom: 0;
+  opacity: 0;
+  transition: 0.3s ease-out;
+}
+
+.company_name {
+  color: rgb(134, 134, 134);
+  margin-left: 10px;
+}
+
+.job {
+  margin-right: 10px;
+}
+
+.text-title {
+  font-size: 1.5em;
+  font-weight: bold;
+}
+
+/*Text*/
+.work_name {
+  font-size: 1.5em;
+  font-weight: bold;
+  margin-left: 10px;
+}
+
+.work_salary {
+  margin-right: 10px;
+  font-size: 1.5em;
+  font-weight: bold;
+  color: red;
+}
+
+/*Hover*/
+.card:hover {
+  border-color: #008bf8;
+  box-shadow: 0 4px 18px 0 rgba(0, 0, 0, 0.25);
+}
+
+.card:hover .card-button {
+  transform: translate(-50%, 50%);
+  opacity: 1;
+}
+</style>
--- src/views/research/index.vue
+++ src/views/research/index.vue
@@ -4,14 +4,53 @@
         <div class="search">
             <SearchBar />
         </div>
+        <div class="sort">
+            <el-button type="primary" style="margin: auto 20px;">名企优先</el-button>
+            <el-button type="primary" style="margin: auto 20px;">薪资优先</el-button>
+            <el-button type="primary" style="margin: auto 20px;">同城优先</el-button>
+            <el-button type="primary" style="margin: auto 20px;">技能优先</el-button>
+
+            <el-button type="primary" style="margin: auto 20px;" @click="drawer = true">
+                详细信息
+            </el-button>
+
+            <el-drawer v-model="drawer" direction="rtl" size="60%">
+                <template #header>
+                    <h1>职位具体信息</h1>
+                </template>
+                <template #default>
+                    <Info />
+                </template>
+                <template #footer>
+                    <div style="flex: auto">
+                        <el-button @click="cancelClick">cancel</el-button>
+                        <el-button type="primary" @click="confirmClick">confirm</el-button>
+                    </div>
+                </template>
+            </el-drawer>
+        </div>
         <div class="center">
             <div class="left">
-                <div class="card"></div>
-                <div class="card"></div>
-                <div class="card"></div>
-                <div class="card"></div>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <div style="margin: 10px auto;">
+                    <el-pagination :page-size="10" :pager-count="7" layout="prev, pager, next" :total="100" />
+                </div>
             </div>
-            <div class="right"></div>
+
+
+            <div class="right">
+
+
+            </div>
         </div>
     </div>
 </template>
@@ -20,14 +59,22 @@
 import { ref } from 'vue'
 import HeaderBar from "@/components/HeaderBar.vue"
 import SearchBar from "@/components/SearchBar.vue"
+import Card from "./components/Card.vue";
+
 </script>
 
 <style scoped>
 .container {
-    margin-top: 60px;
+    margin-top: 50px;
     background-color: rgb(242, 242, 245);
-    height: 92vh;
+    height: calc(100vh-50px);
     overflow-y: auto;
+}
+
+.sort {
+    display: flex;
+    margin: 20px auto 20px 150px;
+    height: 50px;
 }
 
 .search {
@@ -44,7 +91,7 @@ import SearchBar from "@/components/SearchBar.vue"
 }
 
 .left {
-    border: 2px black solid;
+    /* border: 2px black solid; */
     border-radius: 20px;
     display: flex;
     flex-direction: column;
@@ -57,15 +104,16 @@ import SearchBar from "@/components/SearchBar.vue"
     border-radius: 20px;
     display: flex;
     flex: 1;
-    margin: 0 20px 0 10px;
+    margin: 0 20px 60px 10px;
 }
 
 .card {
     border: 2px black solid;
     border-radius: 20px;
     margin: 10px auto;
-    height: 200px;
+    height: 150px;
     /* width: 700px; */
     width: 95%;
+    padding: 10px;
 }
 </style>
