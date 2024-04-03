8593e456dcdb4168585e1d8d9c533bf76378d506
Xiaohm6 <1276214348@qq.com>
2024-04-03 17:37:21 +0800

    更改背景颜色 增加热门岗位推荐

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,43 @@
 ﻿<template>
   <n-layout>
     <HeaderBar />
+    <img src="../../assets/home.png" alt="" style="margin: 50px 0 0 0;width: 100vw;height: 150px;">
     <n-layout-content content-style="padding: 24px;">
       <div class="centerPosition" style="margin-top: 20px">
         <SearchBar />
+        <div class="hotwork">
+          热门职位：
+          <a href="" class="work">
+            python
+          </a>
+          <a href="" class="work">
+            ml工程师
+          </a>
+          <a href="" class="work">
+            运维
+          </a>
+          <a href="" class="work">
+            测试
+          </a>
+          <a href="" class="work">
+            Java
+          </a>
+          <a href="" class="work">
+            C#
+          </a>
+          <a href="" class="work">
+            深度学习
+          </a>
+          <a href="" class="work">
+            教师
+          </a>
+          <a href="" class="work">
+            电子
+          </a>
+        </div>
       </div>
       <FunctionCard />
       <h1>热门职位</h1>
-      <!-- <TagBar /> -->
 
       <Tag />
       <div class="get-more">
@@ -36,9 +66,33 @@ h1 {
 .centerPosition {
   align-items: center;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: auto;
+}
+
+.hotwork {
+  display: flex;
+  margin: 10px auto;
+}
+
+.work {
+  display: flex;
+  margin: 0 10px;
+  border: 2px rgb(38, 133, 161) solid;
+  border-radius: 10px;
+  padding: 0 10px;
+  color: rgb(48, 83, 96);
+  /* 添加默认背景颜色或者透明背景 */
+  background-color: transparent;
+  text-decoration: none;
+}
+
+/* 鼠标悬停时添加渐变背景 */
+.work:hover {
+  background: rgb(140, 209, 230);
+  color: white;
 }
 
 .n-layout {
@@ -50,13 +104,17 @@ h1 {
 .n-layout-content {
   /* flex: 1; */
   /* height: 90%; */
-  height: calc(100vh-50px);
+  height: calc(100vh-140px);
   /* background: rgba(167, 167, 167, 0.277); */
-  background-color: rgb(242, 244, 247);
+  /* background: linear-gradient(to bottom, rgb(186, 236, 255), rgb(164, 216, 236),
+      rgb(191, 228, 242), rgb(205, 227, 235), rgb(210, 233, 241), white); */
+  background: linear-gradient(to bottom, rgba(192, 230, 245, 0.818), rgba(188, 228, 244, 0.616),
+      rgb(211, 238, 248), rgb(221, 239, 245), rgb(225, 238, 242), white);
+  /* background-color: rgb(242, 244, 247); */
   /* background: rgb(225, 222, 222); */
   display: flex;
   flex-direction: column;
-  margin-top: 50px;
+  margin-top: -10px;
 }
 
 .get-more {
