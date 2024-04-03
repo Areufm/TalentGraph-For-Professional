2e740825d1c789b2410184ed37bc71aafc6af3bd
Xiaohm6 <1276214348@qq.com>
2024-04-03 17:39:28 +0800

    增加知识图谱节点说明 更新按钮样式

--- src/views/recommend_work/index.vue
+++ src/views/recommend_work/index.vue
@@ -2,11 +2,20 @@
   <HeaderBar />
   <div class="container">
     <div class="sort">
-      <p style="font-size: 20px; font-weight: bold;">请选择排序方式:</p>
-      <el-button type="primary" style="margin: auto 20px;">名企优先</el-button>
+      <p style="font-size: 20px; font-weight: bold;">请选择推荐方式:</p>
+      <sort-button type="primary" style="margin: auto 20px;">名企优先</sort-button>
+      <sort-button type="primary" style="margin: auto 20px;">薪资优先</sort-button>
+      <sort-button type="primary" style="margin: auto 20px;">同城优先</sort-button>
+      <sort-button type="primary" style="margin: auto 20px;">技能优先</sort-button>
+      <p class="title_job">岗位</p>
+      <p class="title_salary">薪资</p>
+      <p class="title_region">地区</p>
+      <p class="title_skill">技能</p>
+      <p class="title_company">公司</p>
+      <!-- <el-button type="primary" style="margin: auto 20px;">名企优先</el-button>
       <el-button type="primary" style="margin: auto 20px;">薪资优先</el-button>
       <el-button type="primary" style="margin: auto 20px;">同城优先</el-button>
-      <el-button type="primary" style="margin: auto 20px;">技能优先</el-button>
+      <el-button type="primary" style="margin: auto 20px;">技能优先</el-button> -->
 
       <!-- <el-button type="primary" style="margin: auto 20px;" @click="drawer = true">
         详细信息
@@ -32,8 +41,8 @@
         <Card :toggle-drawer="toggleDrawer" />
         <Card :toggle-drawer="toggleDrawer" />
         <Card :toggle-drawer="toggleDrawer" />
-        <div style="margin: 10px auto;">
-          <el-pagination :page-size="4" :pager-count="7" layout="prev, pager, next" :total="40" />
+        <div style="margin: 20px auto 0 auto;">
+          <el-pagination :page-size="4" :pager-count="5" layout="prev, pager, next" :total="40" />
         </div>
       </div>
       <div class="right">
@@ -63,7 +72,7 @@ import HeaderBar from "@/components/HeaderBar.vue";
 import Info from "./components/Info.vue";
 import Relationship from "@/components/Chart/Relationship.vue"
 
-import { ElMessage, ElMessageBox } from 'element-plus'
+// import { ElMessage, ElMessageBox } from 'element-plus'
 
 const drawer = ref(false)
 
@@ -100,11 +109,276 @@ function confirmClick() {
 </script>
 
 <style scoped>
+.title_company {
+  font-size: 20px;
+  color: royalblue;
+  font-weight: 600;
+  letter-spacing: -1px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding-left: 30px;
+  margin: auto 20px;
+}
+
+.title_company::before,
+.title_company::after {
+  position: absolute;
+  content: "";
+  height: 16px;
+  width: 16px;
+  border-radius: 50%;
+  left: 0px;
+  background-color: royalblue;
+}
+
+.title_company::before {
+  width: 18px;
+  height: 18px;
+  background-color: royalblue;
+}
+
+.title_company::after {
+  width: 18px;
+  height: 18px;
+  animation: pulse 1s linear infinite;
+}
+
+.title_job {
+  font-size: 20px;
+  color: rgb(238, 178, 94);
+  font-weight: 600;
+  letter-spacing: -1px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding-left: 30px;
+  margin: auto 20px;
+}
+
+.title_job::before,
+.title_job::after {
+  position: absolute;
+  content: "";
+  height: 16px;
+  width: 16px;
+  border-radius: 50%;
+  left: 0px;
+  background-color: rgb(238, 178, 94);
+}
+
+.title_cotitle_jobmpany::before {
+  width: 18px;
+  height: 18px;
+  background-color: rgb(238, 178, 94);
+}
+
+.title_job::after {
+  width: 18px;
+  height: 18px;
+  animation: pulse 1s linear infinite;
+}
+
+.title_salary {
+  font-size: 20px;
+  color: #FF0000;
+  font-weight: 600;
+  letter-spacing: -1px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding-left: 30px;
+  margin: auto 20px;
+}
+
+.title_salary::before,
+.title_salary::after {
+  position: absolute;
+  content: "";
+  height: 16px;
+  width: 16px;
+  border-radius: 50%;
+  left: 0px;
+  background-color: #FF0000;
+}
+
+.title_salary::before {
+  width: 18px;
+  height: 18px;
+  background-color: #FF0000;
+}
+
+.title_salary::after {
+  width: 18px;
+  height: 18px;
+  animation: pulse 1s linear infinite;
+}
+
+.title_region {
+  font-size: 20px;
+  color: #FF1493;
+  font-weight: 600;
+  letter-spacing: -1px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding-left: 30px;
+  margin: auto 20px;
+}
+
+.title_region::before,
+.title_region::after {
+  position: absolute;
+  content: "";
+  height: 16px;
+  width: 16px;
+  border-radius: 50%;
+  left: 0px;
+  background-color: #FF1493;
+}
+
+.title_region::before {
+  width: 18px;
+  height: 18px;
+  background-color: #FF1493;
+}
+
+.title_region::after {
+  width: 18px;
+  height: 18px;
+  animation: pulse 1s linear infinite;
+}
+
+.title_skill {
+  font-size: 20px;
+  color: rgb(0,
+      206,
+      209);
+  font-weight: 600;
+  letter-spacing: -1px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding-left: 30px;
+  margin: auto 20px;
+}
+
+.title_skill::before,
+.title_skill::after {
+  position: absolute;
+  content: "";
+  height: 16px;
+  width: 16px;
+  border-radius: 50%;
+  left: 0px;
+  background-color: rgb(0, 206, 209);
+}
+
+.title_skill::before {
+  width: 18px;
+  height: 18px;
+  background-color: rgb(0, 206, 209);
+}
+
+.title_skill::after {
+  width: 18px;
+  height: 18px;
+  animation: pulse 1s linear infinite;
+}
+
+@keyframes pulse {
+  from {
+    transform: scale(0.9);
+    opacity: 1;
+  }
+
+  to {
+    transform: scale(1.8);
+    opacity: 0;
+  }
+}
+
+sort-button {
+  position: relative;
+  padding: 10px 20px;
+  border-radius: 7px;
+  border: 2px solid rgb(61, 106, 255);
+  font-size: 15px;
+  text-transform: uppercase;
+  font-weight: 600;
+  letter-spacing: 2px;
+  background: transparent;
+  color: #000000;
+  overflow: hidden;
+  box-shadow: 0 0 0 0 transparent;
+  -webkit-transition: all 0.2s ease-in;
+  -moz-transition: all 0.2s ease-in;
+  transition: all 0.2s ease-in;
+}
+
+sort-button:hover {
+  color: white;
+  background: rgb(61, 106, 255);
+  box-shadow: 0 0 30px 5px rgba(0, 142, 236, 0.815);
+  -webkit-transition: all 0.2s ease-out;
+  -moz-transition: all 0.2s ease-out;
+  transition: all 0.2s ease-out;
+}
+
+sort-button:hover::before {
+  -webkit-animation: sh02 0.5s 0s linear;
+  -moz-animation: sh02 0.5s 0s linear;
+  animation: sh02 0.5s 0s linear;
+}
+
+sort-button::before {
+  content: '';
+  display: block;
+  width: 0px;
+  height: 86%;
+  position: absolute;
+  top: 7%;
+  left: 0%;
+  opacity: 0;
+  background: #04242a;
+  box-shadow: 0 0 50px 30px #fff;
+  -webkit-transform: skewX(-20deg);
+  -moz-transform: skewX(-20deg);
+  -ms-transform: skewX(-20deg);
+  -o-transform: skewX(-20deg);
+  transform: skewX(-20deg);
+}
+
+@keyframes sh02 {
+  from {
+    opacity: 0;
+    left: 0%;
+  }
+
+  50% {
+    opacity: 1;
+  }
+
+  to {
+    opacity: 0;
+    left: 100%;
+  }
+}
+
+button:active {
+  box-shadow: 0 0 0 0 transparent;
+  -webkit-transition: box-shadow 0.2s ease-in;
+  -moz-transition: box-shadow 0.2s ease-in;
+  transition: box-shadow 0.2s ease-in;
+}
+
 .container {
   margin: 0 auto;
   background-color: #fff;
   padding: 50px 30px 0 30px;
   box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+  background: linear-gradient(to bottom, rgba(192, 230, 245, 0.818), rgba(188, 228, 244, 0.616),
+      rgb(211, 238, 248), rgb(221, 239, 245), rgb(225, 238, 242), white);
 }
 
 .sort {
@@ -128,7 +402,7 @@ function confirmClick() {
 .left {
   display: flex;
   flex-direction: column;
-  width: 250px;
+  width: 300px;
   /* margin-left: 30px; */
   overflow-y: auto;
   /* height: 60vh; */
