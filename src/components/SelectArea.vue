f8db726ecee09a842c9e6beee492a10d34d6055e
Xiaohm <1276214348@qq.com>
2024-04-12 00:36:32 +0800

    封装选择地区和职位

---
+++ src/components/SelectArea.vue
@@ -0,0 +1,37 @@
+﻿<template>
+    <el-cascader placeholder="请选择地区" size="default" :options="regionData" v-model="selectedOptions"
+        @change="handleChange" >
+    </el-cascader>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { regionData } from "element-china-area-data";
+
+const selectedOptions = ref(["11", "1101", "110101"]); // 初始化选中值
+
+function findLabelByValue(data, value) {
+    for (const item of data) {
+        if (item.value === value) {
+            return item.label;
+        } else if (Array.isArray(item.children) && item.children.length > 0) {
+            const foundLabel = findLabelByValue(item.children, value);
+            if (foundLabel) {
+                return foundLabel;
+            }
+        }
+    }
+    return "";
+}
+
+const lastSelectedRegionText = computed(() => {
+    const lastOption = selectedOptions.value[selectedOptions.value.length - 1];
+    return findLabelByValue(regionData, lastOption);
+});
+
+const handleChange = (value) => {
+    console.log(value);
+};
+</script>
+
+<style scoped></style>
