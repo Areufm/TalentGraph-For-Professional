ecb8fb91d2d74d98b7631f885e200f6bb527394b
Areufm <1276214348@qq.com>
2024-08-20 21:12:43 +0800

    refactor: Update HeaderBar.vue to display user's name dynamically

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -105,7 +105,7 @@
         </div>
       </div>
       <a href="/profile" class="header_right_text">个人信息</a>
-      <p class="header_right_text">猫猫鼠</p>
+      <p class="header_right_text">{{ authStore.userInfo.name }}</p>
       <el-popover :width="50"
         popper-style="box-shadow: rgb(14 18 22 / 35%) 0px 10px 38px -10px, rgb(14 18 22 / 20%) 0px 10px 20px -15px; padding: 10px;text-align: center">
         <template #reference>
