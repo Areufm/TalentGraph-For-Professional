15abbb434c85766e69629dfc11958992c3ce3488
Areufm <1276214348@qq.com>
2024-08-20 21:09:06 +0800

    refactor: 封装pinia，引入持久化插件

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,10 +1,7 @@
 ﻿import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate"; //引入持久化插件
 
 const stores = createPinia();
+stores.use(piniaPluginPersistedstate); //将插件添加到 pinia 实例上
 
-// 全局注册 store
-export function setupStore(app) {
-  app.use(stores);
-}
-
-export { stores };
+export default stores;
