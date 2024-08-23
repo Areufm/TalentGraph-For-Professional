2c1113362f6aafd325d53b4bb9c0083181d159bc
Areufm <1276214348@qq.com>
2024-08-23 19:18:53 +0800

    refactor: 改写store为setup模式，更符合vue3组合式api

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -59,11 +59,14 @@
 // });
 
 import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export const useAuthStore = defineStore("auth", {
-  state: () => ({
-    isLogin: false,
-    userInfo: {
+// 使用setup模式
+export const useAuthStore = defineStore(
+  "auth",
+  () => {
+    let isLogin = ref(false);
+    const userInfo = ref({
       name: "",
       age: "",
       phone: "",
@@ -79,22 +82,74 @@ export const useAuthStore = defineStore("auth", {
       work: "",
       skill: [],
       info: "",
-    },
-  }),
-  actions: {
-    login() {
-      this.isLogin = true;
+    });
+
+    const login = () => {
+      isLogin = true;
       localStorage.setItem("isLoggedIn", "true");
       // 登录逻辑
-    },
-    logout() {
-      this.isLogin = false;
+    };
+    const logout = () => {
+      isLogin = false;
       localStorage.removeItem("isLoggedIn");
+      localStorage.removeItem("userInfo");
       // 登出逻辑
-    },
-    setUserInfo(newUserInfo) {
-      Object.assign(this.userInfo, newUserInfo);
-    },
+    };
+    const setUserInfo = (newUserInfo) => {
+      Object.assign(userInfo, newUserInfo);
+    };
+
+    return {
+      isLogin,
+      userInfo,
+      login,
+      logout,
+      setUserInfo,
+    };
   },
-  persist: true,
-});
+  {
+    persist: {
+      enabled: true,
+      key: "auth", // 保存的key
+    },
+  }
+);
+
+// export const useAuthStore = defineStore("auth", {
+//   state: () => ({
+//     isLogin: false,
+//     userInfo: {
+//       name: "",
+//       age: "",
+//       phone: "",
+//       email: "",
+//       area: "",
+//       education: [
+//         {
+//           school_name: "",
+//           school_time: ["", ""],
+//           school_degree: "",
+//         },
+//       ],
+//       work: "",
+//       skill: [],
+//       info: "",
+//     },
+//   }),
+//   actions: {
+//     login() {
+//       this.isLogin = true;
+//       localStorage.setItem("isLoggedIn", "true");
+//       // 登录逻辑
+//     },
+//     logout() {
+//       this.isLogin = false;
+//       localStorage.removeItem("isLoggedIn");
+//       // 登出逻辑
+//     },
+//     setUserInfo(newUserInfo) {
+//       Object.assign(this.userInfo, newUserInfo);
+//     },
+//   },
+//   persist: true,
+// });
