911fc60f33854d8bd0848dc41db1593a2438217e
Areufm <1276214348@qq.com>
2024-08-24 19:04:16 +0800

    refactor: 封装pinia且全局导入

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import stores from "./stores";
+import { setupStore } from "./stores";
 
 const app = createApp(App);
-
-app.use(stores);
+setupStore(app);
 app.use(router);
 app.mount("#app");
--- src/stores/index.js
+++ src/stores/index.js
@@ -4,4 +4,8 @@ import piniaPluginPersistedstate from "pinia-plugin-persistedstate"; //引入持
 const stores = createPinia();
 stores.use(piniaPluginPersistedstate); //将插件添加到 pinia 实例上
 
-export default stores;
+// 全局注册 store
+export function setupStore(app) {
+  app.use(stores);
+}
+export { stores };
