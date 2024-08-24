5cb775f5c99bf4b1be80feab03544ba1fbc1bd9b
Areufm <1276214348@qq.com>
2024-08-24 19:03:45 +0800

    refactor: 修改mock文件夹位置，适配mock数据

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+import { viteMockServe } from "vite-plugin-mock";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
@@ -10,13 +10,12 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import Icons from "unplugin-icons/vite";
 import IconsResolver from "unplugin-icons/resolver";
 
-import { viteMockServe } from "vite-plugin-mock";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     viteMockServe({
-      mockPath: "mock", // 存放mock数据的文件夹
+      mockPath: "./mock", // 存放mock数据的文件夹
       localEnabled: true, // 是否启用本地 mock
       prodEnabled: false, // 生产环境禁用 mock
       logger: true, // 启用日志输出，便于调试
