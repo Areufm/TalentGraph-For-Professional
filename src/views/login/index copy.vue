71826d6cf801f472b73c8b0531881b4b6fa79976
x0r <1276214348@qq.com>
2024-10-06 17:49:16 +0800

    refactor：优化样式为自定义样式，配置登录函数功能

--- src/views/login/index copy.vue
+++ src/views/login/index copy.vue
@@ -1,58 +1,124 @@
-﻿<template>
-  <h1 style="position: absolute; top: 7%; left: 10%; color: royalblue">
-    欢迎使用职业猫CareerCat用户端
-  </h1>
+﻿<!-- <template>
   <div class="login">
     <div class="left">
-      <img src="../../assets/login.png" alt="" style="" />
+      <button style="margin: 5px auto" @click="isLogin = !isLogin">
+        我要招聘
+      </button>
+      <button style="margin: 5px auto" @click="isLogin = !isLogin">
+        我要求职
+      </button>
     </div>
     <div class="right">
-      <form class="form" @submit.prevent="handleLogin">
+      <div v-if="isLogin">
+        <Login />
+      </div>
+      <div v-else>
+        <Register />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Login from "@/components/Login.vue";
+import Register from "@/components/Register.vue";
+
+const isLogin = ref(true);
+</script>
+
+<style scoped>
+.login {
+  /* width: 80%; */
+  /* max-width: 500px; */
+  width: 700px;
+  display: flex;
+  border: 2px black solid;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.left {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  margin: 5px;
+  border: 2px black solid;
+}
+.right {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 5px;
+  border: 2px black solid;
+  flex: 2;
+}
+</style> -->
+<template>
+  <h1 style="position: absolute; top: 7%; left: 10%;color: royalblue;">欢迎使用职业猫CareerCat用户端</h1>
+  <div class="login">
+    <div class="left">
+      <img src="../../assets/login.png" alt="" style=" ">
+    </div>
+    <div class="right">
+      <!-- <form class="form">
         <p class="title">Login</p>
         <p class="message">登录职业猫CareerCat</p>
 
         <label>
-          <input
-            required
-            type="text"
-            v-model="LoginForm.username"
-            class="input"
-          />
+          <input required="" placeholder="" type="text" class="input" />
           <span>用户名</span>
         </label>
 
         <label>
-          <input
-            required
-            :type="passwordType"
-            v-model="LoginForm.password"
-            class="input"
-          />
+          <input required="" placeholder="" :type="passwordType" class="input" />
           <span>密码</span>
-          <el-icon class="icon-right" @click="togglePassword">
+          <el-icon class="icon-right" @click="showPwd">
             <i-ep-view v-if="isShow" />
             <i-ep-hide v-else />
           </el-icon>
         </label>
+        <button class="submit" @click="handleLogin">登录</button>
+        <p class="signin">暂时没有账户 ? <a href="/register">去注册</a></p>
+        <p class="signin">暂不注册 <a href="/">返回首页</a></p>
+      </form> -->
+      <el-form :model="LoginForm" :rules="loginRules" ref="loginFormRef" label-width="80px" class="form">
+        <p class="title">Login</p>
+        <p class="message">登录职业猫CareerCat</p>
 
-        <button class="submit" type="submit">登录</button>
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="LoginForm.username" placeholder="请输入用户名" class="input" />
+        </el-form-item>
+
+
+        <el-form-item label="密码" prop="password">
+          <el-input :type="passwordType" v-model="LoginForm.password" placeholder="请输入密码" class="input" show-password/>
+          
+        </el-form-item>
+
+        <el-button type="primary" class="submit" @click="handleLogin">
+          登录
+        </el-button>
 
         <p class="signin">暂时没有账户? <a href="/register">去注册</a></p>
         <p class="signin">暂不注册 <a href="/">返回首页</a></p>
-      </form>
+      </el-form>
     </div>
   </div>
+
+
 </template>
 
 <script setup>
 import { ref, reactive } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { validUsername } from "@/utils/validate";
-import { useAuthStore } from "@/stores/auth";
+import { useAuthStore } from '@/stores/auth'
 import { login } from "@/api/user";
 import { storage } from "@/utils/storage";
 
-const authStore = useAuthStore();
+const authStore = useAuthStore()
 
 const validateUsername = (rule, value, callback) => {
   if (!validUsername(value)) {
@@ -86,15 +152,8 @@ const passwordType = ref("password");
 const redirect = ref(undefined);
 const route = useRoute();
 const router = useRouter();
-const isShow = ref(false);
 
-// 切换密码显示/隐藏
-const togglePassword = () => {
-  isShow.value = !isShow.value;
-  passwordType.value = isShow.value ? "text" : "password";
-};
 
-// 处理登录
 const handleLogin = () => {
   console.log("Loginform:", LoginForm.value);
   login(LoginForm.value)
@@ -103,7 +162,7 @@ const handleLogin = () => {
       storage.set("accessToken", data.accessToken); //accessToken是后端返回的token
       storage.set("refreshToken", data.refreshToken);
       console.log(data);
-      authStore.login();
+      authStore.login()
       router.push("/form");
       ElNotification({
         title: "登录成功！",
@@ -171,7 +230,7 @@ const handleLogin = () => {
         border: 1px solid rgba(105, 105, 105, 0.397);
         border-radius: 10px;
 
-        & + span {
+        &+span {
           position: absolute;
           left: 10px;
           top: 15px;
@@ -181,19 +240,19 @@ const handleLogin = () => {
           transition: 0.3s ease;
         }
 
-        &:placeholder-shown + span {
+        &:placeholder-shown+span {
           top: 15px;
           font-size: 0.9em;
         }
 
-        &:focus + span,
-        &:valid + span {
+        &:focus+span,
+        &:valid+span {
           top: 30px;
           font-size: 0.7em;
           font-weight: 600;
         }
 
-        &:valid + span {
+        &:valid+span {
           color: green;
         }
       }
