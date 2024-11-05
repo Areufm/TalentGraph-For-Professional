1101c4b0cf5648d105442b67cd59e8c3345e179c
x0r <1276214348@qq.com>
2024-11-05 14:22:30 +0800

    refactor：删除原先js代码

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import { setupStore } from "./stores";
-
-const app = createApp(App);
-setupStore(app);
-app.use(router);
-app.mount("#app");
