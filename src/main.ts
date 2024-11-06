9b4a7f3bc2c25ac5a3b1fdd87d3b23f796deed63
x0r <1276214348@qq.com>
2024-11-06 16:14:16 +0800

    refactor：新增全局样式，消除默认样式，规范样式

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { setupStore } from "./store";
+import '@/assets/style/global.scss';
 
 const app = createApp(App);
 setupStore(app);
