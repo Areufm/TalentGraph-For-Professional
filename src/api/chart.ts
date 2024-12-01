ea796a1d35714dc1ea089d2119828a89950b9a2e
x0r <1276214348@qq.com>
2024-12-01 20:17:16 +0800

    feat：实现知识图谱使用接口请求数据

--- src/api/chart.ts
+++ src/api/chart.ts
@@ -1,5 +1,23 @@
 import request from "@/utils/request";
 
+// 获取推荐岗位知识图谱数据
+export const getRecommendGraph = (data: any) => {
+  return request({
+    url: "/api/chart/getRecommendGraph",
+    method: "post",
+    data,
+  });
+};
+
+// 获取个人知识图谱数据
+export const getUserGraph = () => {
+  return request({
+    url: "/api/chart/getUserGraph",
+    method: "get",
+  });
+};
+
+// 获取个人雷达图数据
 export const getRadar = () => {
   return request({
     url: "/api/chart/getRadar",
@@ -7,16 +25,10 @@ export const getRadar = () => {
   });
 };
 
+// 获取全国岗位数量数据
 export const getChina = () => {
   return request({
     url: "/api/chart/getChina",
-    method: "get",
-  });
-};
-
-export const getUserGraph = () => {
-  return request({
-    url: "/api/chart/getUserGraph",
     method: "get",
   });
 };
