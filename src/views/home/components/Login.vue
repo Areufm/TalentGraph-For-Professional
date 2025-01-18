653b45d6f431a97717fd5ebb1826ba94616b11ae
Areufm <1276214348@qq.com>
2025-01-18 13:51:41 +0800

    refactor：优化主页登录框快捷登录功能

--- src/views/home/components/Login.vue
+++ src/views/home/components/Login.vue
@@ -1,16 +1,26 @@
 ﻿<template>
-  <form class="form">
+  <form class="form" @submit.prevent="handleLogin">
     <h3 style="margin-top: auto">登录职业猫CareerCat</h3>
 
     <label>
-      <input required placeholder="" type="text" class="input" />
+      <input required type="text" v-model="LoginForm.username" class="input" />
       <span>用户名</span>
     </label>
 
     <label>
-      <input required placeholder="" type="password" class="input" />
+      <input
+        required
+        :type="passwordType"
+        v-model="LoginForm.password"
+        class="input"
+      />
       <span>密码</span>
+      <el-icon class="icon-right" @click="togglePassword">
+        <i-ep-view v-if="isShow" />
+        <i-ep-hide v-else />
+      </el-icon>
     </label>
+
     <button class="submit" @click="handleLogin">登录</button>
     <p class="signin">暂时没有账户 ? <a href="/register">去注册</a></p>
   </form>
@@ -21,7 +31,8 @@ import { ref, reactive } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { validUsername } from "@/utils/validate";
 import { useAuthStore } from "@/store/auth";
-import { ElNotification } from "element-plus";
+import { login } from "@/api/user";
+import { storage } from "@/utils/storage";
 
 const authStore = useAuthStore();
 
@@ -41,10 +52,9 @@ const validatePassword = (rule: any, value: string, callback: Function) => {
   }
 };
 
-const LoginForm = reactive({
+const LoginForm = ref({
   username: "",
   password: "",
-  password_confirm: "",
 });
 
 const loginRules = reactive({
@@ -54,27 +64,39 @@ const loginRules = reactive({
   ],
 });
 
-const isShow = ref(true);
+const isShow = ref(false);
 const passwordType = ref("password");
 const redirect = ref(undefined);
 const route = useRoute();
 const router = useRouter();
 
-const showPwd = () => {
-  passwordType.value = passwordType.value === "password" ? "" : "password";
+// 切换密码显示/隐藏
+const togglePassword = () => {
   isShow.value = !isShow.value;
+  passwordType.value = isShow.value ? "text" : "password";
 };
 
 const handleLogin = () => {
-  ElNotification({
-    title: "登录成功！",
-    message: "恭喜你成功登录",
-    type: "success",
-    offset: 50,
-  });
-  authStore.login();
-  console.log(authStore.isLogin);
-  router.push("/form");
+  // console.log("LoginForm:", LoginForm.value);
+
+  login(LoginForm.value)
+    .then((res) => {
+      const { data } = res; //data是后端返回的数据
+      storage.set("accessToken", data.accessToken); //accessToken是后端返回的token
+      storage.set("refreshToken", data.refreshToken);
+      authStore.login();
+      router.push("/form");
+      ElNotification({
+        title: "登录成功！",
+        message: "恭喜你成功登录",
+        type: "success",
+        offset: 50,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      ElMessage.error("登陆失败");
+    });
 };
 </script>
 
@@ -85,24 +107,70 @@ const handleLogin = () => {
   flex-direction: column;
   gap: 10px;
   max-width: 400px;
-  /* background-color: #ffffff; */
   padding: 30px;
   border: 2px rgba(137, 191, 230, 0.822) solid;
   border-radius: 20px;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.5);
   margin: 0 auto;
-}
 
-.signin {
-  color: rgba(88, 87, 87, 0.822);
-  font-size: 14px;
-  text-align: center;
+  label {
+    position: relative;
 
-  a {
-    color: royalblue;
+    .icon-right {
+      position: absolute;
+      right: 10px;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+
+    .input {
+      width: 100%;
+      padding: 10px 10px 20px 10px;
+      outline: 0;
+      border: 1px solid rgba(105, 105, 105, 0.397);
+      border-radius: 10px;
+
+      & + span {
+        position: absolute;
+        left: 10px;
+        top: 15px;
+        color: grey;
+        font-size: 0.9em;
+        cursor: text;
+        transition: 0.3s ease;
+      }
+
+      &:placeholder-shown + span {
+        top: 15px;
+        font-size: 0.9em;
+      }
+
+      &:focus + span,
+      &:valid + span {
+        top: 30px;
+        font-size: 0.7em;
+        font-weight: 600;
+      }
+
+      &:valid + span {
+        color: green;
+      }
+    }
+  }
+
+  .submit {
+    width: 100%;
+    border: none;
+    outline: none;
+    background-color: royalblue;
+    padding: 10px;
+    border-radius: 10px;
+    color: #fff;
+    font-size: 16px;
+    transition: 0.3s ease;
 
     &:hover {
-      text-decoration: underline royalblue;
+      background-color: rgb(56, 90, 194);
     }
   }
 }
@@ -111,60 +179,6 @@ const handleLogin = () => {
   display: flex;
   width: 100%;
   gap: 6px;
-}
-
-.form label {
-  position: relative;
-
-  .input {
-    width: 95%;
-    padding: 10px 10px 20px 10px;
-    outline: 0;
-    border: 1px solid rgba(105, 105, 105, 0.397);
-    border-radius: 10px;
-    background-color: transparent;
-
-    & + span {
-      position: absolute;
-      left: 10px;
-      top: 15px;
-      color: grey;
-      font-size: 0.9em;
-      cursor: text;
-      transition: 0.3s ease;
-    }
-
-    &:placeholder-shown + span {
-      top: 15px;
-      font-size: 0.9em;
-    }
-
-    &:focus + span,
-    &:valid + span {
-      top: 30px;
-      font-size: 0.7em;
-      font-weight: 600;
-    }
-
-    &:valid + span {
-      color: green;
-    }
-  }
-}
-
-.submit {
-  border: none;
-  outline: none;
-  background-color: royalblue;
-  padding: 10px;
-  border-radius: 10px;
-  color: #fff;
-  font-size: 16px;
-  transform: 0.3s ease;
-
-  &:hover {
-    background-color: rgb(56, 90, 194);
-  }
 }
 
 @keyframes pulse {
